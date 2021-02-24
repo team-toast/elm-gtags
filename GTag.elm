7d5b8ba98f83b56ec6e448e728acfced90ca0074
coinop-logan <coinop.logan@gmail.com>
2021-02-24 10:07:12 +0000

    Add nonrepeating utilities

--- GTag.elm
+++ GTag.elm
@@ -2,6 +2,14 @@ port module GTag exposing (GTagData, gTagOut)
 
 import Json.Decode
 import Json.Encode
+import List.Extra
+import Set exposing (Set)
+
+
+type alias GTagHistory =
+    { sentOnlyOnce : Set String
+    , sentOnlyOnChange : List GTagData
+    }
 
 
 type alias GTagData =
@@ -35,6 +43,63 @@ encodeGTag gtag =
                 |> Json.Encode.int
           )
         ]
+
+
+gTagOutOnlyOnLabelOrValueChange : GTagHistory -> GTagData -> ( GTagHistory, Cmd msg )
+gTagOutOnlyOnLabelOrValueChange gtagHistory gtag =
+    let
+        hasEvent event =
+            .event >> (==) event
+
+        maybeLastLogged =
+            gtagHistory.sentOnlyOnChange
+                |> List.filter (hasEvent gtag.event)
+                |> List.head
+
+        -- updateAndSend gtag =
+    in
+    case maybeLastLogged of
+        Nothing ->
+            ( { gtagHistory
+                | sentOnlyOnChange =
+                    gtagHistory.sentOnlyOnChange
+                        |> List.append
+                            [ gtag ]
+              }
+            , gTagOut gtag
+            )
+
+        Just lastLogged ->
+            if gtag.label == lastLogged.label && gtag.value == lastLogged.value then
+                ( gtagHistory
+                , Cmd.none
+                )
+
+            else
+                ( { gtagHistory
+                    | sentOnlyOnChange =
+                        gtagHistory.sentOnlyOnChange
+                            |> List.Extra.setIf
+                                (hasEvent gtag.event)
+                                gtag
+                  }
+                , gTagOut gtag
+                )
+
+
+gTagOutOnlyOnceForEvent : GTagHistory -> GTagData -> ( GTagHistory, Cmd msg )
+gTagOutOnlyOnceForEvent gtagHistory gtag =
+    if Set.member gtag.event gtagHistory.sentOnlyOnce then
+        ( gtagHistory, Cmd.none )
+
+    else
+        ( { gtagHistory
+            | sentOnlyOnce =
+                gtagHistory.sentOnlyOnce
+                    |> Set.insert gtag.event
+          }
+        , gTagOut gtag
+        )
 
 
 gTagOut : GTagData -> Cmd msg
